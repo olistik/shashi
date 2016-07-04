1d8a938056cc80bf2c788b87f2956969a4a16912
Maurizio De Magnis <root@olisti.co>
2016-07-04 09:26:46 +0200

    refactors the command into a separate module

---
+++ lib/shashi/command/setup.rb
@@ -0,0 +1,28 @@
+module Shashi
+  module Command
+    module Setup
+
+      def self.perform(file:)
+        prepare_directory(file: file)
+        Utils.write_database(file: file, data: {})
+      end
+
+      private
+
+        def self.prepare_directory(file:)
+          dirname = File.dirname(file)
+          if File.exists?(dirname)
+            if File.directory?(dirname)
+              # ok
+            else
+              puts "You specified #{dirname} as a directory but a file with that name already exists."
+              exit 1
+            end
+          else
+            FileUtils.mkdir_p(dirname)
+          end
+        end
+
+    end
+  end
+end
--- lib/shashi/utils.rb
+++ lib/shashi/utils.rb
@@ -1,7 +1,13 @@
 module Shashi
   module Utils
+
     def self.blank?(value)
       value == "" || value == nil
     end
+
+    def self.write_database(file:, data:)
+      File.write(path, JSON.pretty_generate(data))
+    end
+
   end
 end
